afb747c2435ecb522c68da59d323af5a53de1802
Raees <raeesmulla535@gmail.com>
2023-10-22 22:14:59 +0530

    Create HeaderComponent.js

---
+++ crud/src/component/HeaderComponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      <header>
+        <nav className='narbar navbar-expand-md navbar-dark bg-dark'>
+            <a href='#' className='navbar-brand'>Crud</a>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default HeaderComponent
