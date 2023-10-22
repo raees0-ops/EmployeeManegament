906365a6c3a8e8c546d6f4b25644c4f2fcd56b52
Raees <raeesmulla535@gmail.com>
2023-10-22 22:14:55 +0530

    Create FooterCompomemts.js

---
+++ crud/src/component/FooterCompomemts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterCompomemts = () => {
+  return (
+    <div>
+      <footer className='footer'>
+        <span className='text-danger'>hi raees</span>
+      </footer>
+    </div>
+  )
+}
+
+export default FooterCompomemts
